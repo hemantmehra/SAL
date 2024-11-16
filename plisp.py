48382921aa36b34343bb4d339e92470debc2d739
Hemant Mehra <mehra.hemant14@gmail.com>
2024-11-16 14:15:35 +0530

    add parse_statements, parse_ast and run_function

--- plisp.py
+++ plisp.py
@@ -1,49 +1,29 @@
-from enum import Enum
+iota_counter = 0
 
-class Token(Enum):
-    Op = 1
-    PFunc = 2
-    Lit = 3
+def iota(reset=False) -> int:
+    global iota_counter
+    if reset:
+        iota_counter = 0
+    result = iota_counter
+    iota_counter += 1
+    return result
 
-def _Op(val):
-    return (Token.Op, val)
+Token_Open = iota(True)
+Token_Close = iota()
+Token_Ident = iota()
+Token_Literal = iota()
 
-def _PFunc(val):
-    return (Token.PFunc, val)
+def token_open():
+    return (Token_Open,)
 
-def _Lit(val):
-    return (Token.Lit, val)
+def token_close():
+    return (Token_Close,)
 
-def apply(f, args):
-    if f[0] == Token.Op:
-        if f[1] == '+':
-            return sum([i[1] for i in args])
-        else:
-            assert False, "Assert not reached!\n"
-    else:
-        assert False, "Assert not reached!\n"
+def token_ident(x):
+    return (Token_Ident, x)
 
-def eval(l):
-    if type(l) == list:
-        # print(l[0])
-        return apply(eval(l[0]), [eval(i) for i in l[1:]])
-    
-    elif type(l) == tuple:
-        if l[0] == Token.Op:
-            return l
-        elif l[0] == Token.Lit:
-            return l
-        else:
-            assert False, "Assert not reached!\n"
-    else:
-            assert False, "Assert not reached!\n"
-
-TokenType = Enum('TokenType', [
-    'Open',
-    'Close',
-    'Symbol',
-    'Literal'
-])
+def token_literal(x):
+    return (Token_Literal, x)
 
 def tokenize(code: str) -> list:
     i = 0
@@ -53,15 +33,15 @@ def tokenize(code: str) -> list:
     word = ''
     while i < n:
         if code[i] == '(':
-            tokens.append(TokenType.Open)
+            tokens.append(token_open())
         elif code[i] == ')':
             if word:
-                tokens.append((TokenType.Symbol, word))
+                tokens.append(token_literal(word))
                 word = ''
                 in_word_flag = False
-            tokens.append(TokenType.Close)
+            tokens.append(token_close())
         elif code[i].isspace() and in_word_flag:
-            tokens.append((TokenType.Symbol, word))
+            tokens.append(token_literal(word))
             in_word_flag = False
             word = ''
         elif code[i].isspace() and not in_word_flag:
@@ -72,33 +52,112 @@ def tokenize(code: str) -> list:
         i+=1
     return tokens
 
-def parse(tokens: list) -> list:
+def parse_statements(tokens: list) -> list:
     i = 0
     n = len(tokens)
     stack = []
-    ast = []
+    statements = []
     while i < n:
-        if tokens[i] == TokenType.Open:
+        if tokens[i][0] == Token_Open:
             stack.append([])
-        elif (tokens[i]) == TokenType.Close:
+        elif tokens[i][0] == Token_Close:
             l = stack.pop()
             if len(stack) != 0:
                 stack[-1].append(l)
             else:
-                ast.append(l)
-        elif type(tokens[i]) == tuple and (tokens[i][0]) == TokenType.Symbol:
+                statements.append(l)
+        elif tokens[i][0] == Token_Ident or tokens[i][0] == Token_Literal:
             stack[-1].append(tokens[i])
         else:
-            assert False, "Assert not reached!\n"
+            assert False, f"Assert not reached!, Token: {tokens[i]}\n"
+        i += 1
+    return statements
+
+AST_Func = iota(True)
+AST_Func_Body = iota()
+
+def statement_start_of_func(statement):
+    return statement[0][0] == Token_Ident and statement[0][1] == 'def'
+
+def statement_end_of_func(statement):
+    return statement[0][0] == Token_Ident and statement[0][1] == 'end'
+
+from copy import deepcopy
+
+def parse_ast(statements):
+    i = 0
+    n = len(statements)
+    ast = []
+    func = {}
+    func_block = []
+    in_func_flag = False
+    while i < n:
+        if statement_start_of_func(statements[i]):
+            in_func_flag = True
+            func_block = []
+        elif statement_end_of_func(statements[i]):
+            func['type'] = AST_Func
+            func['name'] = statements[i][1][1]
+            func['block'] = deepcopy(func_block)
+            ast.append(deepcopy(func))
+            func = {}
+            func_block = []
+            in_func_flag = False
+        elif in_func_flag:
+            func_block.append(statements[i])
+        else:
+            assert False, "Assert not reached"
         i += 1
     return ast
 
-with open('test.lsp') as fin:
-    source_code = fin.read()
+def apply(f, args):
+    if f[0] == Token_Ident and f[1] == 'print':
+        print(args[0][1])
+    else:
+        assert False, "Assert not reached!\n"
 
-    tokens = tokenize(source_code)
-    # for i in tokens:
-    #     print(i)
-    ast = parse(tokens)
-    for i in ast:
-        print(i)
+def eval(l):
+    if type(l) == list:
+        return apply(eval(l[0]), [eval(i) for i in l[1:]])
+    
+    elif type(l) == tuple:
+        if l[0] == Token_Literal:
+            return l
+        elif l[0] == Token_Ident:
+            return l
+        else:
+            assert False, "Assert not reached!\n"
+    else:
+            assert False, "Assert not reached!\n"
+
+def run_function(ast, func_name):
+    for func in ast:
+        print(func['name'])
+        if func['name'] == func_name:
+            for block in func['block']:
+                eval(block)
+            return
+    assert False, f'Assert not reached. Function {func_name} not found.'
+
+
+tokens = [
+    token_open(),
+    token_ident('def'),
+    token_ident('main'),
+    token_close(),
+    token_open(),
+    token_ident('print'),
+    token_literal(1),
+    token_close(),
+    token_open(),
+    token_ident('end'),
+    token_ident('main'),
+    token_close()
+]
+statements = parse_statements(tokens)
+print(statements)
+
+ast = parse_ast(statements)
+print(ast)
+
+run_function(ast, 'main')
